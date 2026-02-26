2f0f13b1932731a3c981bde15fd3fa074095435e
Guillermo Arevalo Fernandez <guillermo.arevalo.fdz@gmail.com>
2026-02-26 13:02:52 +0000

    Requirement 1: Added Column class

---
+++ src/main/java/uk/ac/ucl/model/Column.java
@@ -0,0 +1,33 @@
+package uk.ac.ucl.model;
+
+import java.util.ArrayList;
+
+public class Column {
+    private final String name;
+    private final ArrayList<String> rows;
+
+    public Column(String name) {
+        this.name = name;
+        rows = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getSize() {
+        return this.rows.size();
+    }
+
+    public String getRowValue(int idx) {
+        return rows.get(idx);
+    }
+
+    public void setRowValue(String value, int idx) {
+        rows.set(idx, value);
+    }
+
+    public void addRowValue(String value) {
+        rows.add(value);
+    }
+}
